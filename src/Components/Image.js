d425a1fd0df6b522fa42307a6be636767826428e
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-02-20 18:08:28 -0600

    added styled components

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -3,11 +3,17 @@ import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button
   } from 'reactstrap';
+  import styled from "styled-components";
 
+  const ImageStyle = styled(CardImg) `
+  border: solid 8px paleturquoise;
+  width: 95%;
+
+  `
 const Image = props => {
     return (
         <Card className="daily-img" key={props.id}>
-            <CardImg top width="100%" className="nasa-img" alt="NASA Image" src={props.image} />
+            <ImageStyle top width="100%" className="nasa-img" alt="NASA Image" src={props.image} />
             <CardBody>
                 <CardTitle>{props.title}</CardTitle>
                 <CardSubtitle>{props.date}</CardSubtitle>
